928d1df2a9b54848d599f64f3fb31931f3c0dfb5
Glong <873136927@qq.com>
2023-04-18 23:59:21 +0800

    add axios request and fix cross domain problem

--- src/components/EmailValidation.vue
+++ src/components/EmailValidation.vue
@@ -2,20 +2,46 @@
     <div class="email-div">
         <div class="emil-div-send">
             <label>请输入邮箱号：</label>
-            <input class="input-style" id="email-input" name="email-input" type="text">
-            <button class="btn-style">发送验证码</button>
+            <input class="input-style" id="email-input" name="email-input" type="text" v-model="email" placeholder="请输入邮箱号">
+            <button class="btn-style" @click="sendCode">发送验证码</button>
         </div>
         <div class="email-div-valid">
             <label>请输入验证码：</label>
-            <input class="input-style" id="code-input" name="code-input" type="text">
+            <input class="input-style" id="code-input" name="code-input" type="text" placeholder="请输入验证码">
             <button class="btn-style">确定</button>
         </div>
     </div>
 </template>
 
 <script>
+import request from '@/utils/request';
     export default {
-        
+        data(){
+            return {
+                email: ""
+            }
+        },
+        methods: {
+            sendCode(){
+                const urlEmail = "/email/send/" + this.email;
+                console.log(urlEmail);
+                request({
+                    method: 'Get',
+                    url: urlEmail
+                })
+                request.get(urlEmail)
+                .then(function (response){
+                    console.log(response)
+                })
+                .catch(function (error) {
+                    // 处理错误情况
+                    console.log("error");
+                })
+                .then(function () {
+                    // 总是会执行
+                });
+            }
+        }
     }
 </script>
 
